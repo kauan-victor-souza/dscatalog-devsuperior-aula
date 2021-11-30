5a05a5554d78e6e091fdb0a3aeff85484602bd0a
Kauan Souza <k1one.g.vitor@gmail.com>
2021-11-30 12:04:45 -0300

    Update category

--- backend/src/main/java/com/dvsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/dvsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dvsuperior.dscatalog.dto.CategoryDTO;
 import com.dvsuperior.dscatalog.entities.Category;
 import com.dvsuperior.dscatalog.repository.CategoryRepository;
-import com.dvsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.dvsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -30,18 +32,35 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id){
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	
 	}
 
 	
-	@Transactional(readOnly = true)
+	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
 		Category entity = new Category();	
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	
+	}
+
+	
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());		
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		
+		} 
+		
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id "+ id +" not found!");
+		}
+			
 	}
 }
