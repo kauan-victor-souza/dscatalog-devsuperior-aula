44ebfd56b3cc4e613fd1c9c19e147bf3acafa829
Kauan Souza <k1one.g.vitor@gmail.com>
2022-10-12 16:48:51 -0300

    Fix: Postgres compatibility

--- backend/src/main/java/com/dvsuperior/dscatalog/repository/ProductRepository.java
+++ backend/src/main/java/com/dvsuperior/dscatalog/repository/ProductRepository.java
@@ -1,11 +1,22 @@
 package com.dvsuperior.dscatalog.repository;
 
+import com.dvsuperior.dscatalog.entities.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dvsuperior.dscatalog.entities.Product;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Query("SELECT DISTINCT obj FROM Product obj " +
+            "INNER JOIN  obj.categories cats " +
+            "WHERE (COALESCE(:categories) IS NULL OR cats IN :categories) " +
+            "AND (LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')) ) ")
+    Page<Product> find(List<Category> categories, String name, Pageable pageable);
 }
--- backend/src/main/java/com/dvsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/dvsuperior/dscatalog/services/ProductService.java
@@ -1,5 +1,7 @@
 package com.dvsuperior.dscatalog.services;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,8 +35,9 @@ public class ProductService {
 	
 	
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged(Pageable pageable){
-		Page<Product> list = repository.findAll(pageable);	
+	public Page<ProductDTO> findAllPaged(Long categoryId, String name, PageRequest pageRequest){
+		List<Category> categories = (categoryId == 0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
+		Page<Product> list = repository.find(categories, name, pageRequest);
 		return list.map(x -> new  ProductDTO(x));
 	}
 	
